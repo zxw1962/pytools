31f8071bf000ae5f5d729fd587156a840da23478
Xiaowei Zhao <zxw1962@126.com>
2016-01-05 23:03:20 +0800

    refine weather.py by dump city name:id pairs to file

--- weather.py
+++ weather.py
@@ -5,7 +5,8 @@ import urllib
 import urllib2
 import json
 import xml.etree.ElementTree as ET
-import time
+import cPickle
+import os.path
 
 # 以下link来自中华万年历抓包,哇哈哈...
 #http://wthrcdn.etouch.cn/weather_mini?city=北京
@@ -18,40 +19,41 @@ import time
 #以下来自魅族自带天气抓包,支持同时获得多个城市天气
 #http://aider.meizu.com/app/weather/listWeather?cityIds=101210101&cityIds=101010100
 
+
 def unicodetoutf8(unicodestr):
     return unicodestr.encode('utf-8')
+
 
 def utf8tounicode(utf8str):
     return utf8str.decode('utf-8')
     # unicode(utf8str, encoding='utf-8')
 
-def getcityidfromname1(name):
-    tree = ET.parse('/Users/xiaoweiz/pytools/allcities.xml')
-    root = tree.getroot()
-    counties = root.iter('county')
-    for item in counties:
-        xmlname = unicodetoutf8(item.attrib['name'])
-        if name == xmlname:
-            return unicodetoutf8(item.attrib['weatherCode'])
-    return ''
 
-def getcityidfromname(names, cityIds):
-    # TODO(xiaoweiz): 有点慢啊,需要优化下啊!!!
-    # 看来得搞个city name --> city id 的dict
-    tree = ET.parse('/Users/xiaoweiz/pytools/allcities.xml')
-    root = tree.getroot()
-    counties = root.iter('county')
-    for item in counties:
-        xmlname = unicodetoutf8(item.attrib['name'])
-        if xmlname in names:
-            cityIds.append(('cityIds', unicodetoutf8(item.attrib['weatherCode'])))
-            if len(cityIds) == len(names):
-                break
-    return {}
+def constructcitynameidpairs():
+    dict_file = '/Users/xiaoweiz/pytools/allcities.dict'
+    allcities = {}
+    if not os.path.exists(dict_file):
+        tree = ET.parse('/Users/xiaoweiz/pytools/allcities.xml')
+        root = tree.getroot()
+        counties = root.iter('county')
+        for item in counties:
+            xmlname = unicodetoutf8(item.attrib['name'])
+            weathercode = item.attrib['weatherCode']
+            allcities[xmlname] = weathercode
+
+        with open(dict_file, 'wb') as output_file:
+            cPickle.dump(allcities, output_file)
+    else:
+        with open(dict_file, 'rb') as input_file:
+            allcities = cPickle.load(input_file)
+
+    return allcities
+
 
 def handlerealtime(realtime):
     print u'实时: %s°, %s, %s, %s, 空气湿度%s%%' % (realtime['temp'], realtime['wD'], realtime['wS'],
                                               realtime['weather'], realtime['sD'])
+
 
 def handlefuture(futureweather):
     print '未来几天天气:'
@@ -60,6 +62,7 @@ def handlefuture(futureweather):
         print u'%s %s %s 气温%s° ~ %s° %s %s' % (weather['date'], weather['week'], weather['weather'],
                                                weather['temp_day_c'], weather['temp_night_c'],
                                                weather['wd'], weather['ws'])
+
 
 def handlepm25(pm25):
     print u'空气质量: %s, aqi = %s, pm25 = %s' % (pm25['quality'], pm25['aqi'], pm25['pm25'])
@@ -70,22 +73,18 @@ if __name__ == "__main__":
     params = sys.argv[1:]
     paramsLen = len(params)
     if paramsLen == 0:
-        print 'you need provide at least one city name!!!'
-        sys.exit(1)
+        # default to 杭州 if none supplied
+        params.append('杭州')
 
     query = []
-    time1 = time.time()
-    getcityidfromname(params, query)
-    # for name in params:
-    #     cityId = getcityidfromname1(name)
-    #     if cityId != '':
-    #         query.append(('cityIds', cityId))
-    #     else:
-    #         print 'cannot find city id for city = ' + name
-    # print 'used time = %s' % (time.time()-time1)
+    citynameiddict = constructcitynameidpairs()
+    for name in params:
+        try:
+            cityId = citynameiddict[name]
+            query.append(('cityIds', cityId))
+        except KeyError:
+            print 'cannot find city id for city = ' + name
     # print query
-    if len(query) == 0:
-        sys.exit(1)
     final_url = weather_api + urllib.urlencode(query)
     response = urllib2.urlopen(final_url)
     js = json.load(response)
